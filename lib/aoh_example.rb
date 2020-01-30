432e135d503f7d0edc0518071d6849dfd7654d03
Christopher Leja <christopher.leja@gmail.com>
2020-01-30 00:04:31 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,10 @@ def assembled_aoh
 end
 
 def literal_aoh
-  nest = [{:name => "Don Gately", :occupation => "Live-in Staff"}, {:name => "Pat Monteseian", :occupation => "Staff"}, {:name => "Kate Gompert", :occupation => "None"}, {:name => "Bruce Green", :occupation => "Fan of Mildred"}]
+  nest = [{:name=>"Don Gately", :occupation=>"Live-in Staff"}, 
+          {:name=>"Pat Monteseian", :occupation=>"Staff"}, 
+          {:name=>"Kate Gompert", :occupation=>"None"}, 
+          {:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
 end
 
 def aoh_lookup(aoh, row, key)
